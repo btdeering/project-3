d2e5e31431278cbd6dd34e4a76757c7e15f45433
ZalakG <jhalakgajjar@hotmail.com>
2023-07-12 11:39:13 -0400

    changes

--- Covid_data/Covid_data.js
+++ Covid_data/Covid_data.js
@@ -4,6 +4,22 @@
   // create a layer to add the markers
   let layerGroup = L.layerGroup().addTo(map);
 
+  // let regionPolygonData =  {
+  //   type:"FeatureCollection",
+  //   features:[
+        // {
+        //     "type":"Feature",
+        // "id":"01",
+        // "properties":
+        // {"name":"Alabama",
+        // },
+        // "geometry":
+        // {"type":"Polygon",
+        // "coordinates":[[]]
+        // }
+        // }
+//]
+ //};
 // Adding the tile layer
 	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		maxZoom: 19,
@@ -51,7 +67,37 @@ const year = L.control();
 
 	year.addTo(map);
 
+	function getColor(d) {
+    return d > 15000 ? '#800026' :
+    d > 12000  ? '#BD0026' :
+    d > 10000  ? '#E31A1C' :
+    d > 7000  ? '#FC4E2A' :
+    d > 5000   ? '#FD8D3C' :
+    d > 3000   ? '#FEB24C' :
+    d > 1000   ? '#FED976' : '#FFEDA0';
+	}
 
+  const legend = L.control({position: 'bottomright'});
+
+	legend.onAdd = function (map) {
+
+		const div = L.DomUtil.create('div', 'info legend');
+		const grades = [1000, 3000, 5000, 7000, 10000, 12000, 15000];
+		const labels = [];
+		let from, to;
+
+		for (let i = 0; i < grades.length; i++) {
+			from = grades[i];
+			to = grades[i + 1];
+
+			labels.push(`<i style="background:${getColor(from + 1)}"></i> ${from}${to ? `&ndash;${to}` : '+'}`);
+		}
+
+		div.innerHTML = labels.join('<br>');
+		return div;
+	};
+
+	legend.addTo(map);
 // function to call when the year and region values are selected 
 function selectYearandRegion(){ 
 const year = document.getElementById('year').value; // get the selected year value 
@@ -72,8 +118,31 @@ layerGroup.clearLayers();
   }
 
   //load the JSON file and get the data 
-  d3.json(link).then(function(data) {
- let filteredData = data;
+  d3.json(link).then(function(data) { 
+
+//     let transformedData = data.reduce((acc, d) => { 
+//       if (Object.keys(acc).includes(d.Region)) return acc;
+  
+//       acc[d.Region] = data.filter(g => g.Region === d.Region); 
+//       return acc;
+//   }, {})
+// let index = 0;
+//   for(let region of Object.entries(transformedData)) {
+//     const feature = {
+//            type:"Feature",
+//         id: index++,
+//         properties:
+//         {name:region[0],
+//         },
+//         geometry:
+//         {type:"Polygon",
+//         coordinates:[[]]
+//     }
+//       region[1]
+
+//   }
+
+  let filteredData = data;
     if(region !== 'All') {
       //filter the data for the selected region 
       filteredData = data.filter(val=> val.Region === region);
@@ -81,11 +150,35 @@ layerGroup.clearLayers();
 
     //loop through the filtered data and add markers on the map and its details for the in the popup.
     filteredData.map(value => {
-      L.marker([value.lat, value.long]).addTo(layerGroup).bindPopup(`<h2>${value.State}</h2><hr>
+
+      const myCustomColour = getColor(value.total_deaths);
+      const markerHtmlStyles = `
+  background-color: ${myCustomColour};
+  width: 2rem;
+  height: 2rem;
+  display: block;
+  left: -1.5rem;
+  top: -1.5rem;
+  position: relative;
+  border-radius: 2rem 2rem 0;
+  transform: rotate(45deg);
+  border: 1px solid #FFFFFF`
+  
+      const myIcon = L.divIcon({
+        className: "my-custom-pin",
+        iconAnchor: [0, 24],
+        labelAnchor: [-6, 0],
+        popupAnchor: [0, -36],
+        html: `<span style="${markerHtmlStyles}" />`
+      })
+      
+
+      L.marker([value.lat, value.long],{icon: myIcon}).addTo(layerGroup).bindPopup(`<h2>${value.State}</h2><hr>
       <b>Total Confirmed: </b><i>${value.total_confirmed}</i>
       <br><b>Total Deaths: </b><i>${value.total_deaths}</i>
       <br><b>Average Fatality Ratio:</b> <i>${value.average_case_fatality_ratio}</i>`);
     });
+    
 });
   
 }
@@ -93,7 +186,6 @@ layerGroup.clearLayers();
 
 //calling function on the load to load data of year 2021 for all regions as default. 
   selectYearandRegion();
-
 
 
 
